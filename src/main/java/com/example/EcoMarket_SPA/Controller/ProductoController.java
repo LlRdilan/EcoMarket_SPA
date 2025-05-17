27f0739307d44dcf3f23f0398b5960f8a109fcda
dilan <di.llanquileo@duocuc.cl>
2025-05-17 18:41:50 -0400

    Creación de todas las clases

---
+++ src/main/java/com/example/EcoMarket_SPA/Controller/ProductoController.java
@@ -0,0 +1,4 @@
+package com.example.EcoMarket_SPA.Controller;
+
+public class ProductoController {
+}
---
+++ src/main/java/com/example/EcoMarket_SPA/Repository/TiendaRepository.java
@@ -0,0 +1,4 @@
+package com.example.EcoMarket_SPA.Repository;
+
+public class TiendaRepository {
+}
