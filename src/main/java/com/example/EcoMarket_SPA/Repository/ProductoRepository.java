0e221a28a501d1d1c92c3be112a269bcacf323c7
mbgomezz <mb.gomez@duocuc.cl>
2025-05-19 11:58:26 -0400

    Update ProductoRepository.java
    Agregado set, get

--- src/main/java/com/example/EcoMarket_SPA/Repository/ProductoRepository.java
+++ src/main/java/com/example/EcoMarket_SPA/Repository/ProductoRepository.java
@@ -1,4 +1,47 @@
 package com.example.EcoMarket_SPA.Repository;
 
 public class ProductoRepository {
+  
+    private List<Producto> productos = new ArrayList<>();
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public Producto getProducto(int id){
+        for(Producto p: productos){
+            if(p.getId()==id){
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public Producto createProducto(Producto p){
+        productos.add(p);
+        return p;
+    }
+    public boolean deleteProducto(int id){
+        Producto p = this.getProducto(id);
+        if(p==null){
+            return false;
+        }else{
+            productos.remove(p);
+            return true;
+        }
+    }
+    public Producto updateProducto(Producto p) {
+        for(Producto p2 : productos) {
+            if (p.getId() == p2.getId()) {
+                p2.setNombre(p.getNombre());
+                p2.setPrecio(p.getPrecio());
+                p2.setDescripcion(p.getDescripcion());
+                p2.setCantidad(p.getCantidad());
+                return p2;
+            }
+        }
+        return null;
+    }
+}
+
 }
