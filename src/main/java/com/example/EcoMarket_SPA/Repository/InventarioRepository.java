84cbc0747dbbf63fb98b9191a403b0e8ade23ffb
mbgomezz <mb.gomez@duocuc.cl>
2025-05-20 01:13:30 -0400

    Update InventarioRepository.java
    Agregados los set, get

--- src/main/java/com/example/EcoMarket_SPA/Repository/InventarioRepository.java
+++ src/main/java/com/example/EcoMarket_SPA/Repository/InventarioRepository.java
@@ -1,4 +1,44 @@
 package com.example.EcoMarket_SPA.Repository;
-
+import java.util.ArrayList;
+import java.util.List;
 public class InventarioRepository {
+  private List<Inventario> inventario = new ArrayList<>();
+
+    public List<Inventario> getInventario() {
+        return inventario;
+    }
+
+    public Inventario getProducto(int id){
+        for(Inventario i: inventario){
+            if(i.getId()==id){
+                return i;
+            }
+        }
+        return null;
+    }
+
+    public Inventario createProducto(Inventario i){
+        inventario.add(i);
+        return i;
+    }
+    public boolean deleteProducto(int id){
+        Inventario i = this.getInventario(id);
+        if(i==null){
+            return false;
+        }else{
+            inventario.remove(i);
+            return true;
+        }
+    }
+    public Inventario updateProducto(Inventario i) {
+        for(Inventario i2 : inventario) {
+            if (i.getId() == i2.getId()) {
+                i2.setProducto(i.getProducto());
+                i2.setTienda(i.getTienda());
+                return i2;
+            }
+        }
+        return null;
+    }
+    }
 }
