3089d610d434f0f78bcf5064acddd2357a82b410
mbgomezz <mb.gomez@duocuc.cl>
2025-05-22 02:00:15 -0400

    Update ProductoController.java
    actualizado

--- src/main/java/com/example/EcoMarket_SPA/Controller/ProductoController.java
+++ src/main/java/com/example/EcoMarket_SPA/Controller/ProductoController.java
@@ -1,57 +1,40 @@
 package com.example.EcoMarket_SPA.Controller;
 
-import com.example.EcoMarket_SPA.Model.Pedido;
+import com.example.EcoMarket_SPA.Model.Proovedor;
+import com.example.EcoMarket_SPA.Services.ProovedorServices;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
-import com.example.EcoMarket_SPA.Repository.ProductoRepository;
-import com.example.EcoMarket_SPA.Services.PedidoServices;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/pedidos")
-public class PedidoController {
-        @Autowired
-        private PedidoServices pedidoServices;
+@RequestMapping(("/api/proovedores"))
+public class ProovedorController {
+    @Autowired
+    private ProovedorServices proovedorServices;
 
-        @GetMapping
-        public ResponseEntity<List<Pedido>> getAllPedidos() {
-            List<Pedido> pedidos = pedidoServices.getPedidos();
-            return new ResponseEntity<>(pedidos, HttpStatus.OK);
-        }
-
-        @GetMapping("/{id}")
-        public ResponseEntity<Pedido> getPedidoById(@PathVariable int id) {
-            Pedido pedido = pedidoServices.getPedido(id);
-            if (pedido != null) {
-                return new ResponseEntity<>(pedido, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        }
-
-        @PostMapping
-        public ResponseEntity<Pedido> addPedido(@RequestBody Pedido pedido) {
-            Pedido nuevoPedido = pedidoServices.savePedido(pedido);
-            return new ResponseEntity<>(nuevoPedido, HttpStatus.CREATED);
-        }
-
-        @PutMapping("/{id}")
-        public ResponseEntity<Pedido> updatePedido(@PathVariable int id, @RequestBody Pedido pedido) {
-            pedido.setId(id);
-            Pedido actualizado = pedidoServices.updatePedido(pedido);
-            if (actualizado != null) {
-                return new ResponseEntity<>(actualizado, HttpStatus.OK);
-            } else {
-                return new ResponseEntity<>(HttpStatus.NOT_FOUND);
-            }
-        }
-
-        @DeleteMapping("/{id}")
-        public ResponseEntity<Void> deletePedido(@PathVariable int id) {
-            pedidoServices.deletePedido(id);
-            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
-        }
+    @GetMapping
+    public List<Proovedor> getAllProovedores() {
+        return proovedorServices.getProovedor();
     }
+
+    @GetMapping("/{id}")
+    public Proovedor getProovedorById(@PathVariable int id) {
+        return proovedorServices.getProovedor(id);
+    }
+
+    @PostMapping
+    public Proovedor addProovedor(@RequestBody Proovedor proovedor) {
+        return proovedorServices.saveProovedor(proovedor);
+    }
+
+    @PutMapping("/{id}")
+    public Proovedor updateProovedor(@PathVariable int id, @RequestBody Proovedor proovedor) {
+        proovedor.setId(id);
+        return proovedorServices.updateProovedor(proovedor);
+    }
+
+    @DeleteMapping("/{id}")
+    public boolean deleteProovedor(@PathVariable int id) {
+        return proovedorServices.deleteProovedor(id);
+    }
+}
