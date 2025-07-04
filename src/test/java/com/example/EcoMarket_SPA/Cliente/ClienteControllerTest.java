47cf88a093fefb158eabb9ecc47fcdd6d15245de
dilan <di.llanquileo@duocuc.cl>
2025-07-03 21:02:55 -0400

    Linea roja de MockBean, no se quito pero funciona

--- src/test/java/com/example/EcoMarket_SPA/Cliente/ClienteControllerTest.java
+++ src/test/java/com/example/EcoMarket_SPA/Cliente/ClienteControllerTest.java
@@ -7,6 +7,7 @@ import com.example.EcoMarket_SPA.Assemblers.ClienteModelAssemblers;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.CollectionModel;
@@ -21,7 +22,10 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(controllers = ClienteController.class)
+@AutoConfigureMockMvc //Pobre con pero no me resulto nada
+
 public class ClienteControllerTest {
+
 
     @Autowired
     private MockMvc mockMvc;
